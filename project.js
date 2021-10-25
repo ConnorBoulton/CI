c05be900aa435741f8dfde74d9e25e8d6332ca2b
Connor Boulton <cjboulton22@gmail.com>
2021-10-25 16:27:41 -0600

    test added

--- project.js
+++ project.js
@@ -1,5 +1,11 @@
 function getGreeting(name){
-    return "Hi " + name;
+    return "Hi" + name;
 }
 
 module.exports = getGreeting;
+
+function sum(num1, num2){
+    return num1 + num2;
+}
+
+module.exports = sum;
--- test.js
+++ test.js
@@ -1,5 +1,10 @@
-const getGreeting = require('./project');
+const getGreeting = require('./project')
+const sum = require('./project')
+
+test('adds two numbers', () => {
+  expect(sum(10, 10)).toBe(20);
+});
 
 test('can create a basic greeting', () => {
-  expect(getGreeting('Sam')).toBe("Hi Sam");
+  expect(getGreeting("Sam")).toBe("Hi Sam");
 });
